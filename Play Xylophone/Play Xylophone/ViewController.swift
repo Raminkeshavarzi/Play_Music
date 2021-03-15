7bc99188c169149a0ca107af59a099f57f755024
Ramin Keshavarzi <raminkeshavarzi@organiplace.com>
2021-03-15 07:26:31 +0100

    added initial conifg

---
+++ Play Xylophone/Play Xylophone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Play Xylophone
+//
+//  Created by Ramin on 15/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
